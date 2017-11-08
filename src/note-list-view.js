37b4234c62123a9e55cc493b709ae346aa6c8254
Tim Jones <timjones10@hotmail.com>
2017-11-08 11:37:45 +0000

    Updated to hide methods using exports module pattern

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,32 +1,18 @@
 (function(exports) {
-  function NoteListView(noteList = new NoteList) {
-    this.noteList = noteList;
-  }
 
-  NoteListView.prototype.noteToHTML = function() {
-    var arrayOfNotes = this.noteList.showNotes().map(function(note) {
-      return note.getText();
-    });
-    return "<ul><li><div>" + arrayOfNotes.join("</div></li><li><div>") + "/div></li></ul>";
+  exports.NoteListView = function(noteList = new NoteList) {
+    var noteList = noteList;
+
+    noteToHTML = function() {
+      var arrayOfNotes = noteList.showNotes().map(function(note) {
+        return note.getText();
+      });
+      return "<ul><li><div>" + arrayOfNotes.join("</div></li><li><div>") + "/div></li></ul>";
+    };
+
+    return {
+      noteToHTML: noteToHTML,
+      noteList: noteList
+    };
   };
-
-  exports.NoteListView = NoteListView;
 })(this);
-
-// (function(exports) {
-//
-//   exports.NoteListView = function(noteList = new NoteList) {
-//     var noteList = noteList;
-//
-//     noteToHTML = function() {
-//       var arrayOfNotes = noteList.showNotes().map(function(note) {
-//         return note.getText();
-//       });
-//       return "<ul><li><div>" + arrayOfNotes.join("</div></li><li><div>") + "/div></li></ul>";
-//     };
-//
-//     return {
-//       noteToHTML: noteToHTML
-//     };
-//   };
-// })(this);
